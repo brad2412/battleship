37b971451ebdaa71deb889da47a27a2e5550cdcc
Bradley Milton <bmilton2412@gmail.com>
2023-04-07 14:38:27 -0700

    add sunk and hit test

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -7,6 +7,25 @@ RSpec.describe Ship do
       
       expect(cruiser.name).to eq("Cruiser")
       expect(cruiser.length).to eq(3)
+      expect(cruiser.health).to eq(3)
     end
   end
+
+  describe '#sunk?' do
+    it 'has a sunk method' do
+      cruiser = Ship.new("Cruiser", 3)
+
+      expect(cruiser.sunk?).to eq(false)
+    end
+  end
+
+  describe '#hit' do
+    it 'has a hit method' do
+      cruiser = Ship.new("Cruiser", 3)
+
+      cruiser.hit
+      expect(cruiser.health).to eq(2)
+    end
+  end
+
 end
