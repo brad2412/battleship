eb6d3b6ab8743a7e78ed789af06e11145f54808b
William Zale <williamzale@gmail.com>
2023-04-08 16:20:21 -0400

    Lengthened render test to check for 'S' being printed for ship, 'H' for hit, and 'X' for sunk.

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe Cell do
   end
 
   describe '#render' do
-    it 'has a render method' do
+    it 'has a render method with an optional arguement' do
       cell_1 = Cell.new("B4")
       expect(cell_1.render).to eq(".")
 
@@ -55,8 +55,19 @@ RSpec.describe Cell do
       cell_2 = Cell.new("C3")
       cruiser = Ship.new("Cruiser", 3)
       
-      cell_2.place_ship("Cruiser")
+      cell_2.place_ship(cruiser)
       expect(cell_2.render).to eq(".")
+
+      expect(cell_2.render(true)).to eq("S")
+      cell_2.fire_upon
+
+      expect(cell_2.render).to eq("H")
+      expect(cruiser.sunk?).to eq(false)
+      cruiser.hit
+      cruiser.hit
+      expect(cruiser.sunk?).to eq(true)
+
+      expect(cell_2.render).to eq("X")
     end
   end
 end
